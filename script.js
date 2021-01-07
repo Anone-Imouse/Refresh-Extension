6d7df7ae5bbe4ecd424afd4951ef35cc172b04fa
Anone-Imouse <73369548+Anone-Imouse@users.noreply.github.com>
2021-01-07 13:20:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,2 @@
-/*const rando=Math.floor(Math.random() * 5) + 1;
-
-//console.log(rando)
-
-if(rando !== 5){
-   alert('Better luck next time :(');
-    
-    window.location.reload(true)
-}
-else if (rando === 5){
-    alert('Lucky Boi! :)')
-}
-else{
-    alert('Error')
-    for(let i=0; i<100; i++){
-        console.error('Error!!! :(')
-    }
-}*/
-
-document.querySelector('button.VfPpkd-LgbsSe').click()
+alert('Nope, you shall not pass!')
+window.location.reload(true)
